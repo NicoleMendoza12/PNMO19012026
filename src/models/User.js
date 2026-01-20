351a57e259d6ffeb705559a5e20174b17c03db71
Nicole Ordoñez <NicoleMendoza1080@outlook.com>
2026-01-19 22:50:01 -0300

    Versión final lista para entrega - Pruebas exitosas

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs'); 
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -27,19 +27,15 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) return next();
+userSchema.pre('save', async function() {
+    if (!this.isModified('password')) return;
     
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (error) {
-        next(error);
-    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    
 });
 
-// Método para comparar contraseñas
+
 userSchema.methods.comparePassword = async function(candidatePassword) {
     return await bcrypt.compare(candidatePassword, this.password);
 };
